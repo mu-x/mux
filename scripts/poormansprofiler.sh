6ca5180c802254c9521d13ff19efbf8f272e9f02
mu-x <uskov.mikhail@gmail.com>
2017-01-11 18:22:38 +0300

    scripts/poormansprofiler: minor fix

--- scripts/poormansprofiler.sh
+++ scripts/poormansprofiler.sh
@@ -11,22 +11,22 @@ set -e
 COUNT=${C:-10}
 SLEEP=${S:-1}
 
-if [[ "$1" == '^[0-9]+$' ]]; then
+if [[ "$1" == ^[0-9]+$ ]]; then
     PID=$1
 else
-    PID=$(pgrep nx_network_ut | tail -1)
+    PID=$(pgrep $1 | tail -1)
 fi
 
 for N in $(seq 1 $COUNT)
-  do
-    printf '\rAnalyzing process %s ... %s/%s' "$PID" "$N" "$COUNT" >&2
-    gdb -ex "set pagination 0" -ex "thread apply all bt" -batch -p $PID || exit $?
-    sleep $SLEEP
-  done | \
-awk '
-  BEGIN { s = ""; }
-  /^Thread/ { print s; s = ""; }
-  /^\#/ { if (s != "" ) { s = s "," $4} else { s = $4 } }
-  END { print s }' | \
-sort | uniq -c | sort -r -n -k 1,1
+    do
+        printf '\rAnalyzing process %s ... %s/%s' "$PID" "$N" "$COUNT" >&2
+        gdb -ex "set pagination 0" -ex "thread apply all bt" -batch -p $PID || exit $?
+        sleep $SLEEP
+    done \
+| awk '
+    BEGIN { s = ""; }
+    /^Thread/ { print s; s = ""; }
+    /^\#/ { if (s != "" ) { s = s "," $4} else { s = $4 } }
+    END { print s }' \
+| sort | uniq -c | sort -r -n -k 1,1
 echo >&2
