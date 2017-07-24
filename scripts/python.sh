5213218257d530a6563b72b4026fc79d0e291168
mu-x <uskov.mikhail@gmail.com>
2017-07-24 19:06:03 +0300

    scripts/python: add from future support

--- scripts/python.sh
+++ scripts/python.sh
@@ -7,6 +7,7 @@ Configuration:
     P interpretator, default: python
     M import modules (import M), default: sys, os
     I included modules (from I import *), default: math
+    F from __future__ import {d=devision,w=with,u=unicode}
 END
 exit 0; fi
 
@@ -25,8 +26,12 @@ for module in $INCLUDES; do
     IMPORTS="from $module import *; $IMPORTS"
 done
 
+[[ "$F" == *d* ]] && IMPORTS="from __future__ import division; $IMPORTS"
+[[ "$F" == *w* ]] && IMPORTS="from __future__ import with_statement; $IMPORTS"
+[[ "$F" == *u* ]] && IMPORTS="from __future__ import unicode_literals; $IMPORTS"
+
 if [[ "$@" ]]; then
     python -c "$IMPORTS print $@"
 else
-    cat | python
+    cat <(echo -n "print ") <(cat) | python
 fi
