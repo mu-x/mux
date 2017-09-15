d6a5d735eb8f8866e6b61e3f6fcbde8e35dfa5ee
mu-x <uskov.mikhail@gmail.com>
2017-09-15 21:11:24 +0300

    configs/manage.sh improvement

--- configs/manage.sh
+++ configs/manage.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+if [[ "$1" == --help ]] || [[ "$1" == -h ]]; then cat <<END
+Manages configs in user's home directory and repository
+Usage: $0 [status|push|pull]
+END
+exit 0; fi
+
+set -e
+[ $X ] && set -x
+
 cd $(dirname "${BASH_SOURCE[0]}")
 
 function each_rc {
     ACTION="$1"
-    for CONF in $(ls .*rc *.conf .*.conf .*config)
-    do
+    for CONF in $(echo .??*); do
         LOCAL=$HOME/$CONF
         REMOTE=$PWD/$CONF
         echo $ACTION for $CONF
-        eval $ACTION
+        eval $ACTION || true
     done
 }
 
-for ARG in ${@:-status}
-do
+for ARG in ${@:-status}; do
     case $ARG in
-        -h|*help)   echo "<script> [help|status|push|pull]" ;;
         save|push)  each_rc 'cp $LOCAL $REMOTE'             ;;
         load|pull)  each_rc 'cp $REMOTE $LOCAL'             ;;
         *)          each_rc 'diff $LOCAL $REMOTE'           ;;
