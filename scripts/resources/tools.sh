4e331a7198cfeac6b51609a59b12c57f9339e0bb
mu-x <uskov.mikhail@gmail.com>
2017-12-08 16:21:42 +0300

    .muxrc update

--- scripts/resources/tools.sh
+++ scripts/resources/tools.sh
@@ -17,18 +17,7 @@ function mux_silent_run() {
     echo $@
     $@ >/dev/null 2>&1 &
 }
-
-# CD to windows path (assuming drives are mounted to /)
-function mux_windows_cd() {
-    local path=${1//\\/\/}
-    if [ $(cut -c2-2 <<< "$path") == ":" ]; then
-        local drive=/$(cut -c1-1 <<< "$path")
-        path=${drive,,}$(cut -c3- <<< "$path")
-    fi
-    cd "$path"
-}
-
-# Setup BASH cross-terminal history with [limit] 
+# Setup BASH cross-terminal history with [limit]
 function mux_bash_history() {
     export HISTCONTROL=ignoredups:erasedups
     export HISTSIZE=$1
@@ -39,9 +28,23 @@ function mux_bash_history() {
 
 # Run first avalible GUI tool from space separated [list] with [args...]
 function mux_first_gui_tool() {
-    for tool in $1; do
+    tools=$1
+    shift
+    for tool in $tools; do
         which $tool >/dev/null 2>&1 && \
             mux_silent_run $tool "$@" && \
             return 0
     done
 }
+
+# Prints windows [path] as UNIX (assuming drives are mounted to /)
+function mux_windows_path() {
+    local path="${1//\\/\/}"
+    if [ $(cut -c2-2 <<< "$path") == ":" ]; then
+        local drive=/$(cut -c1-1 <<< "$path")
+        path=${drive,,}$(cut -c3- <<< "$path")
+    fi
+    echo "$path"
+}
+
+
