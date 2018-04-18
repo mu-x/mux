1ea929baae8f851c5a6b59057f28c46dd1634af5
mu-x <uskov.mikhail@gmail.com>
2018-04-18 23:00:00 +0300

    scripts/kill: Finds processes by grep and offers to kill them.

---
+++ scripts/kill.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+if [[ "$1" == --help ]] || [[ "$1" == -h ]] || [[ ! "$1" ]]; then cat <<END
+Finds processes by grep and offers to kill them.
+Usage: [PS="ps aux"] $0 PROCCESS_MASK
+END
+exit 0; fi
+
+set -e
+[ $X ] && set -x
+
+NAME=$1
+PS=${PS:-"ps aux"}
+
+LIST=$($PS | grep -v ' grep ' | grep "$NAME")
+# TODO: Print some message if process is not found.
+
+$PS | head -1
+echo "$LIST"
+echo
+read -p 'Select PIDs, blank for everyone: ' PIDS
+
+if [ ! "$PIDS" ]; then
+    if uname -a | grep -q MINGW; then
+        PIDS=$(echo "$LIST" | awk '{print$1}')
+    else
+        PIDS=$(echo "$LIST" | awk '{print$2}')
+    fi
+fi
+
+for PID in $PIDS; do
+    echo Killing PID $PID
+    kill -9 $PID
+done
+
