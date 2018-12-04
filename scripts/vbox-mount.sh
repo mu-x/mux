d35bafe74eaf8ad05c9b94a2cbb3fd1df1275700
mu-x <uskov.mikhail@gmail.com>
2018-12-04 12:39:40 +0300

    scripts/vbox-mount: mount share for current user

---
+++ scripts/vbox-mount.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [[ "$1" == --help ]] || [[ "$1" == -h ]] || [[ ! "$1" ]]; then cat <<END
+Mounts Virtual Box share for current user.
+Usage: $0 SHARE_NAME [MOUNT_PATH]
+END
+exit 0; fi
+
+set -e
+[ $X ] && set -x
+
+SHARE_NAME=$1
+MOUNT_PATH=${2:-/$(echo $SHARE_NAME | tr '[:upper:]' '[:lower:]')}
+echo "Mount $SHARE_NAME to $MOUNT_PATH"
+
+sudo mkdir -p "$MOUNT_PATH"
+sudo chown "$USER:$USER" "$MOUNT_PATH"
+sudo mount -t vboxsf -o uid=$UID "$SHARE_NAME" "$MOUNT_PATH"
