1cc2b12e3337249f5e3f5e982a4721c5d30fdf46
mu-x <uskov.mikhail@gmail.com>
2019-04-24 17:50:07 +0300

    scripts/grep: ignore build dirs

--- scripts/grep.sh
+++ scripts/grep.sh
@@ -2,7 +2,7 @@
 
 if [[ "$1" == --help ]] || [[ "$1" == -h ]]; then cat <<END
 Grep in stream, file or directory skiping version control
-Usage 1: $0 [OPTIONS] PATTERN [FILE|DIRECTORY]
+Usage 1: $0 [OPTIONS] PATTERN [FILE|DIRECTORY [OPTIONS]]
 Usage 2: cat FILE | $0 [OPTIONS] PATTERN
 END
 exit 0; fi
@@ -26,7 +26,9 @@ fi
 
 OPTIONS="$@ --color=always"
 if [[ "$DIR" ]]; then
-    grep -rI "$PATTERN" "$DIR" $OPTIONS --exclude-dir=".hg" --exclude-dir=".git"
+    grep -rI "$PATTERN" "$DIR" $OPTIONS \
+        --exclude-dir=".hg" --exclude-dir=".git" \
+        --exclude-dir="build" --exclude-dir="build-*"
 else
     grep "$PATTERN" $OPTIONS
 fi
