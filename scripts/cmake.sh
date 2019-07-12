c4e1bf06db18323e22cc0a046f99f2bb1ce8eabb
mu-x <uskov.mikhail@gmail.com>
2019-07-12 17:08:25 +0300

    scripts/cmake: generate fix

--- scripts/cmake.sh
+++ scripts/cmake.sh
@@ -18,27 +18,27 @@ set -e
 [ ! "$K" ] && clear
 [ "$X" ] && set -x
 
-GENERATE_FLAGS="$G"
+GENERATE_FLAGS=($G)
 BUILD_DIR=${B:-"$PWD-build"}
 [ "$C" ] && mux_trace_run rm -r "$BUILD_DIR"
 
 BUILD_FLAGS="--build $BUILD_DIR"
 [ "$1" ] && BUILD_FLAGS+=" --target $1"
 
-if [ ! -d "$BUILD_DIR" ]; then
+if [ ! -d "$BUILD_DIR" ] || [ "$GENERATE_FLAGS" ]; then
     mkdir -p "$BUILD_DIR"
-    GENERATE_FLAGS+=" -DCMAKE_CXX_FLAGS=' -fdiagnostics-color=always'"
     if mux_is_windows; then
-        GENERATE_FLAGS+=" -Ax64 -Thost=x64"
+        GENERATE_FLAGS+=(-Ax64 -Thost=x64)
     else
-        GENERATE_FLAGS+=" -GNinja"
+        GENERATE_FLAGS+=(-GNinja)
     fi
+    GENERATE_FLAGS+=(-DCMAKE_CXX_FLAGS=' -fdiagnostics-color=always')
 fi
 
 if [ "$GENERATE_FLAGS" ]; then
 	SRC_DIR="$PWD"
 	cd "$BUILD_DIR"
-	mux_trace_run cmake "$SRC_DIR" $GENERATE_FLAGS
+	mux_trace_run cmake "$SRC_DIR" "${GENERATE_FLAGS[@]}"
 	cd -
 fi
 
