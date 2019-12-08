be2970c231bea700f4b8ff5f19bd26badb04e76e
mu-x <uskov.mikhail@gmail.com>
2019-12-07 16:54:04 -0800

    scripts/vbox-mount: use c drive as default

--- scripts/vbox-mount.sh
+++ scripts/vbox-mount.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [[ "$1" == --help ]] || [[ "$1" == -h ]] || [[ ! "$1" ]]; then cat <<END
 Mounts Virtual Box share for current user.
-Usage: $0 SHARE_NAME [MOUNT_PATH]
+Usage: $0 [SHARE_NAME or c] [MOUNT_PATH or /\$SHARE]
 END
 exit 0; fi
 
 set -e
 [ $X ] && set -x
 
-SHARE_NAME=$1
+SHARE_NAME=${1:c}
 MOUNT_PATH=${2:-/$(echo $SHARE_NAME | tr '[:upper:]' '[:lower:]')}
 echo "Mount $SHARE_NAME to $MOUNT_PATH"
 
